8b190f7e5d867371b7cad30266c8149a1560a1b4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-31 15:34:20 +0530

    failed to push

---
+++ src/main/java/com/onion/AlliumCepaApplication.java
@@ -0,0 +1,13 @@
+package com.onion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlliumCepaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlliumCepaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onion/AlliumCepaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlliumCepaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
