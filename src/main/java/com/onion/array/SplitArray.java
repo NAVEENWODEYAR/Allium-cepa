0ebb63e226a069fa430d8ac5bbd77f9576a11ac1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-31 22:53:55 +0530

    int[] subArray1 = Arrays.copyOfRange(arr,0,4);

--- src/main/java/com/onion/array/SplitArray.java
+++ src/main/java/com/onion/array/SplitArray.java
@@ -1,9 +1,16 @@
 package com.onion.array;
 
+import java.util.Arrays;
+
 public class SplitArray
 {
+    static void splitArray(int[] arr)
+    {
+        System.out.println("\n Original Array given:"+ Arrays.toString(arr));
+        int[] subArray1 = Arrays.copyOfRange(arr,0,4);
+    }
     public static void main(String[] args)
     {
-
+        splitArray(new int[]{1,3,4,5,6,7,8});
     }
 }
