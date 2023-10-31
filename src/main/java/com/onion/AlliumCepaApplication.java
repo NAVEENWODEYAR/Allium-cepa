3f36febc6d6d4c4e224355530ce1f43281ded133
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-31 15:42:12 +0530

    rename master to main

--- src/main/java/com/onion/AlliumCepaApplication.java
+++ src/main/java/com/onion/AlliumCepaApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlliumCepaApplication {
+public class AlliumCepaApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(AlliumCepaApplication.class, args);
+		System.out.println("\n Allium Cepa.,");
 	}
 
 }
