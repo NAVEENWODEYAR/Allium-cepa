672479c622110bc8accc2e1ba73d45a15b70d3ba
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-09 22:10:05 +0530

    log.info("Logging");

--- src/main/java/com/onion/controller/WelcomeController.java
+++ src/main/java/com/onion/controller/WelcomeController.java
@@ -1,14 +1,17 @@
 package com.onion.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class WelcomeController
 {
     @GetMapping
     public String greetMsg()
     {
+        log.info("Logging");
         return "Welcome to the SpringBoot App(Docker Image)";
     }
 }
